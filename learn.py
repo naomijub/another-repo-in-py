f67a3fd2c52f0bdae8facceb96badfc7157f454f
naomijub <jnboeira@outlook.com>
2016-12-24 22:09:09 -0300

    refactor

--- learn.py
+++ learn.py
@@ -13,32 +13,32 @@ class Learn:
         self.error = 0.01
 
     def train(self, think, inNeurons, hidNeurons, outNeurons):
-        currentError = 100000.0
+        currenterror = 100000.0
         while (currentError > self.error):
-            stepError = 0.0
+            steperror = 0.0
             for i in range(len(self.table)):
                 outputs = think.think(self.table[i][0:7])
                 think.clean()
-                outErrors = [float(self.table[i][7]) - float(outputs[0]), float(self.table[i][8]) - float(outputs[1]), float(self.table[i][9]) - float(outputs[2])]
-                error = self.getOutError(outErrors)
-                stepError += error
+                outerrors = [float(self.table[i][7]) - float(outputs[0]), float(self.table[i][8]) - float(outputs[1]), float(self.table[i][9]) - float(outputs[2])]
+                error = self.get_out_error(outErrors)
+                steperror += error
                 self.hidError(self.outError(outErrors, outNeurons, hidNeurons, i), outNeurons, hidNeurons, inNeurons, i)
-            currentError = stepError
-            print(currentError)
+            currenterror = steperror
+            print(currenterror)
 
-    def getOutError(self, outErrors):
+    def get_out_error(self, outerrors):
         error = 0.0
-        for i in outErrors:
+        for i in outerrors:
             error += math.pow(i, 2)
         return error / 6
 
-    def outError(self, outErrors, outNeurons, hidNeurons, var):
+    def out_error(self, outErrors, outNeurons, hidNeurons, var):
         neuronErrors = []
         for i in range(len(outNeurons)):
             error = float(self.table[var][7 + i]) * (1 - float(self.table[var][7 + i])) * outErrors[i]
             for j in range(7):
-                aux_weight = outNeurons[i].getWeights()[j] + self.learnCoef * error * hidNeurons[j].getOutput()
-                outNeurons[i].getWeights()[j] = aux_weight
+                aux_weight = outNeurons[i].get_weights()[j] + self.learnCoef * error * hidNeurons[j].getOutput()
+                outNeurons[i].getW_wights()[j] = aux_weight
             neuronErrors.append(error)
         return neuronErrors
 
@@ -46,8 +46,8 @@ class Learn:
         for i in range(len(hidNeurons)):
             out = hidNeurons[i].getOutput()
             for j in range(len(inNeurons)):
-                error1 = out * (1- out) * neuronErrors[0] * outNeurons[0].getWeights()[j] * inNeurons[j].getOutput()
-                error2 = out * (1- out) * neuronErrors[1] * outNeurons[1].getWeights()[j] * inNeurons[j].getOutput()
-                error3 = out * (1- out) * neuronErrors[2] * outNeurons[2].getWeights()[j] * inNeurons[j].getOutput()
-                aux_weight = hidNeurons[i].getWeights()[i] + self.learnCoef * (error1 + error2 + error3) * inNeurons[j].getOutput()
-                hidNeurons[i].getWeights()[i] = aux_weight
+                error1 = out * (1- out) * neuronErrors[0] * outNeurons[0].get_weights()[j] * inNeurons[j].get_output()
+                error2 = out * (1- out) * neuronErrors[1] * outNeurons[1].get_weights()[j] * inNeurons[j].get_output()
+                error3 = out * (1- out) * neuronErrors[2] * outNeurons[2].get_weights()[j] * inNeurons[j].get_output()
+                aux_weight = hidNeurons[i].get_weights()[i] + self.learnCoef * (error1 + error2 + error3) * inNeurons[j].get_output()
+                hidNeurons[i].get_weights()[i] = aux_weight
--- neuron.py
+++ neuron.py
@@ -12,18 +12,18 @@ class Neuron:
     def weight_init(self, size):
         self.weights = [random.uniform(-1, 1) for i in xrange(size)]
 
-    def setInputs(self, inputs):
+    def set_inputs(self, inputs):
         sum = 0.0
         for i in range(len(inputs)):
             sum += float(inputs[i]) * self.weights[i]
         return sum
 
-    def getWeights(self):
+    def get_weights(self):
         return self.weights
 
     def think(self, inputs):
         self.output = self.pulse(self.setInputs(inputs))
         return self.output
 
-    def getOutput(self):
+    def get_output(self):
         return self.output
